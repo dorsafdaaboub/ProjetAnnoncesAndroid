984d887ec4294a4d6a9b3857f325018848ac0beb
omarfr <omar.foudh@gmail.com>
2016-12-20 22:33:00 +0000

    test

---
+++ ProjetAnnoncesAndroid-master/app/src/main/java/com/isetr/buysell/Controller/NvAnnoce.java
@@ -0,0 +1,15 @@
+package com.isetr.buysell.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.isetr.buysell.R;
+
+public class NvAnnoce extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nv_annoce);
+    }
+}
